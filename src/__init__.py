269d3412e3eb6c5a6e80819042724f6703acdce9
Noah Syrkis <noah@syrkis.com>
2023-05-18 22:55:12 +0200

    plots file

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from src.utils import get_setup, get_files, plot_metrics
 from src.data import get_loaders
 from src.train import train
 from src.model import init_params
+from src.plots import plot_graph
 
 
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     "train",
     "init_params",
     "plot_metrics",
+    "plot_graph"
 ]
--- src/utils.py
+++ src/utils.py
@@ -11,11 +11,6 @@ from tqdm import tqdm
 import pandas as pd
 from matplotlib import pyplot as plt
 import yaml
-import jraph
-import networkx as nx
-import igraph as ig
-import plotly.graph_objects as go
-
 
 
 # PATHS
@@ -190,85 +185,4 @@ def plot_metrics(metrics):
     axes[1].legend()
     plt.show()
 
-
-
-def connectome_to_nx_graph(connectome):
-    # given a jraph graph, return a nx graph
-    nx_graph = nx.Graph()
-    nx_graph.add_nodes_from(range(connectome.n_node))
-    edges = [(int(connectome.senders[i]), int(connectome.receivers[i])) for i in range(connectome.n_edge)]
-    nx_graph.add_edges_from(edges)
-    return nx_graph
-
-def plot_graph(graph: jraph.GraphsTuple) -> None:
-    # visualize the connectome
-    G = ig.Graph.from_networkx(connectome_to_nx_graph(graph))
-    pos = G.layout("kk", dim=3)
-    Xn, Yn, Zn = zip(*pos)
-    Xe, Ye, Ze = [], [], []
-    for e in G.get_edgelist():
-        Xe += [pos[e[0]][0], pos[e[1]][0], None]
-        Ye += [pos[e[0]][1], pos[e[1]][1], None]
-        Ze += [pos[e[0]][2], pos[e[1]][2], None]
-    
-    edge_trace = go.Scatter3d(
-        x=Xe,
-        y=Ye,
-        z=Ze,
-        mode="lines",
-        line=dict(color="rgb(125,125,125)", width=1),
-        hoverinfo="none",
-    )
-
-    node_trace = go.Scatter3d(
-        x=Xn,
-        y=Yn,
-        z=Zn,
-        mode="markers",
-        name="",
-        marker=dict(
-            symbol="circle",
-            size=6,
-            color="rgb(200,200,200)",
-            line=dict(color="rgb(50,50,50)", width=0.5),
-        ),
-        text=list(range(len(Xn))),
-        hoverinfo="text",
-    )
-    axis = dict(
-        showbackground=False,
-        showline=False,
-        zeroline=False,
-        showgrid=False,
-        showticklabels=False,
-        title="",
-    )
-    layout = go.Layout(
-        title="",
-        width=1500,
-        height=1000,
-        # black background
-        paper_bgcolor="rgba(0,0,0,0)",
-        showlegend=False,
-        scene=dict(xaxis=dict(axis), yaxis=dict(axis), zaxis=dict(axis)),
-        margin=dict(t=100),
-        hovermode="closest",
-        annotations=[
-            dict(
-                showarrow=False,
-                text="",
-                xref="paper",
-                yref="paper",
-                x=0,
-                y=0.1,
-                xanchor="left",
-                yanchor="bottom",
-                font=dict(size=14),
-            )
-        ],
-    )
-    data = [edge_trace, node_trace]
-    fig = go.Figure(data=data, layout=layout)
-    fig.show()
-    
 
