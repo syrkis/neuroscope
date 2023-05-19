9d5f756280f13c2704181134d076e5fa1f989a2c
Noah Syrkis <noah@syrkis.com>
2023-05-19 21:00:21 +0200

    todo test

--- src/data.py
+++ src/data.py
@@ -14,12 +14,11 @@ from src.fmri import lh_fmri, rh_fmri, get_multi_roi_mask
 from src.coco import preprocess, get_meta_data, c_to_one_hot
 
 # batch_loader
-# TODO: add k-fold cross validation
-def get_loaders(args, config):  # TODO: add k-fold cross validation
+def get_loaders(args, config): 
     """return a test data loader, and a k-fold cross validation generator"""
     image_size = config["data"]["image_size"]
     meta_data = get_meta_data()
-    images = []
+    images = []  # TODO: testing todo stuff in the middle of a line
     img_files = [f for f in get_files(args.subject) if f.endswith(".png")][
         : args.n_samples
     ]
