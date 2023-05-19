216b0c71d4898539cdf642f541884fb1b8308b66
Noah Syrkis <noah@syrkis.com>
2023-05-19 19:03:17 +0200

    todo test

--- src/data.py
+++ src/data.py
@@ -14,7 +14,8 @@ from src.fmri import lh_fmri, rh_fmri, get_multi_roi_mask
 from src.coco import preprocess, get_meta_data, c_to_one_hot
 
 # batch_loader
-def get_loaders(args, config):  # TODO: allow for loading and mixing multiple subjects
+# TODO test this todo issue thingy
+def get_loaders(args, config):  # TODO allow for loading and mixing multiple subjects
     """return a test data loader, and a k-fold cross validation generator"""
     image_size = config["data"]["image_size"]
     meta_data = get_meta_data()
