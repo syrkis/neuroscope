eb600dde47b3be855a99d336ef3f2e1ac77968db
Noah Syrkis <noah@syrkis.com>
2023-05-19 18:10:03 +0200

    add pylint and fix jobs

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def main():
     """main function"""
     args, config = get_setup()
     k_fold, _ = get_loaders(args, config)
-    rng = jax.random.PRNGKey(0)
-    params = init_params(config, rng)
-    metrics = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
-    params, metrics = train(params, metrics, config, args, k_fold)
+    train_loader, val_loader = next(k_fold)
+    # rng = jax.random.PRNGKey(0)
+    # params = init_params(config, rng)
+    # metrics = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
+    # params, metrics = train(params, metrics, config, args, k_fold)
 
 
 # run main()
