9bb1b0aa26786f2bb5990aee3923205e45125982
Noah Syrkis <noah@syrkis.com>
2023-05-20 15:10:23 +0200

    logging issues

--- src/train.py
+++ src/train.py
@@ -19,30 +19,31 @@ rng = hk.PRNGSequence(jax.random.PRNGKey(42))
 # functions
 def train(k_fold, config):
     """train function"""
-    wandb.init(project="neuroscope", entity='syrkis', config=config)
-    for fold in k_fold:
+    wandb.init(project="neuroscope", entity='syrkis',
+               config=config, group='k_fold')
+    for fold_idx, fold in enumerate(k_fold):
         train_loader, val_loader = fold
         img, cat, _, _, fmri = next(train_loader)
         params = init(next(rng), img, cat)
         opt_state = opt.init(params)
-        params = train_fold(params, train_loader, val_loader, opt_state)
+        params = train_fold(params, train_loader, val_loader, opt_state, fold_idx)
     wandb.finish()
     return params
         
 
-def train_fold(params, train_loader, val_loader, opt_state, steps=1000):
+def train_fold(params, train_loader, val_loader, opt_state, fold_idx, steps=100):
     """train_fold function"""
     pbar = tqdm(range(steps))
     for step in pbar:
         img, cat, sup, cap, fmri = next(train_loader)
         params, opt_state = update(params, img, cat, fmri, opt_state)
-        if step % (steps // 100) == 0:
+        if step % (steps // 10) == 0:
             metrics = evaluate(params, train_loader, val_loader)
-            wandb.log({'step': step, **metrics})
+            wandb.log(metrics, step=fold_idx)
     return params
 
 
-# @jax.jit
+@jax.jit
 def update(params, img, cat, fmri, opt_state):
     grads = grad(loss_fn)(params, img, cat, fmri)
     updates, opt_state = opt.update(grads, opt_state)
