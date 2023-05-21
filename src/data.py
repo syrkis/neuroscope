d3ea2f43c1c78afc57a07916ebcc8826d55132b1
Noah Syrkis <noah@syrkis.com>
2023-05-21 13:10:04 +0200

    change data from generator to array

--- src/data.py
+++ src/data.py
@@ -28,7 +28,7 @@ def get_loaders(args, config):
     train_idxs, test_idxs = map(
         jnp.array, train_test_split(range(len(images)), test_size=0.2, random_state=42)
     )
-    test_loader = get_loader(
+    test_loader = get_data(
         images[test_idxs], args, meta_data, [img_files[idx] for idx in test_idxs]
     )
     return (
@@ -47,36 +47,27 @@ def k_fold_fn(images, args, meta_data, img_files, k=5):
             jnp.array,
             train_test_split(range(len(images)), test_size=0.2, random_state=i),
         )
-        train_loader = get_loader(
+        train_loader = get_data(
             images[train_idxs], args, meta_data, [img_files[idx] for idx in train_idxs]
         )
-        val_loader = get_loader(
+        val_loader = get_data(
             images[val_idxs], args, meta_data, [img_files[idx] for idx in val_idxs]
         )
-        yield train_loader, val_loader
+        return train_loader, val_loader
 
 
-# get batch for subject. TODO: mix subjects?
-def get_loader(images, args, meta_data, img_files):
+def get_data(images, args, meta_data, img_files):
     """return a data loader combining images and fmri data, and adding COCO stuff"""
 
     lh_fmri_roi = lh_fmri[:, get_multi_roi_mask(args.rois, "left")]
     rh_fmri_roi = rh_fmri[:, get_multi_roi_mask(args.rois, "right")]
     fmri = jnp.concatenate((lh_fmri_roi, rh_fmri_roi), axis=1)
 
-    coco_ids = [
-        int(f.split(".")[0].split("-")[-1]) for f in img_files
-    ]  # for getting coco meta data
+    coco_ids = [int(f.split(".")[0].split("-")[-1]) for f in img_files]  # coco meta ids
     cats = meta_data.iloc[coco_ids]["categories"].values  # category info
     cats = jnp.array([c_to_one_hot(c) for c in cats])  # one-hot encoding
-    supers = meta_data.iloc[coco_ids]["supercategory"].values  # supercategory info
-    captions = meta_data.iloc[coco_ids]["captions"].values  # caption info
 
-    while True:
-        perm = np.random.permutation(len(img_files))  # randomize order of images
-        perm = perm[
-            : len(perm) - (len(perm) % args.batch_size)
-        ]  # drop last batch if it's not full
-        for i in range(0, len(perm), args.batch_size):
-            idxs = perm[i : i + args.batch_size]
-            yield images[idxs], cats[idxs], fmri[idxs]  # supers[idxs], captions[idxs], fmri[idxs]
+    perm = np.random.permutation(len(img_files))  # randomize order of images
+    return images[perm], cats[perm], fmri[perm]  # supers[perm], captions[perm], fmri[perm]
+    # supers = meta_data.iloc[coco_ids]["supercategory"].values  # supercategory info
+    # captions = meta_data.iloc[coco_ids]["captions"].values  # caption info
