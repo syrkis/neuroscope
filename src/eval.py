2a9ec5aee4342b4c6fbd12a2a455f32bf2bbb2b9
Noah Syrkis <noah@syrkis.com>
2023-05-22 18:39:10 +0200

    fix eval loss

--- src/eval.py
+++ src/eval.py
@@ -10,9 +10,10 @@ from src.model import train_loss, infer_loss
 
 # evaluate
 def evaluate(params, train_data, val_data, get_batch, config, steps=4):
-    train_loss = train_loss(params, get_batch(train_data, config['batch_size'] * steps))
-    val_loss = infer_loss(params, get_batch(val_data, config['batch_size'] * steps))
-    return {'train_loss': train_loss, 'val_loss': val_loss}
+    return dict(
+        train_loss=train_loss(params, get_batch(train_data, config['batch_size'] * steps)),
+        val_loss=infer_loss(params, get_batch(val_data, config['batch_size'] * steps))
+    )
 
 def save_if_best(params, best_loss, val_loss):
     if val_loss < best_loss:
