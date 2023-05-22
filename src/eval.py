90fbf0eb920e499962eef4bb9dd5797b19f4ca5f
Noah Syrkis <noah@syrkis.com>
2023-05-22 18:30:05 +0200

    update eval

--- src/eval.py
+++ src/eval.py
@@ -9,11 +9,10 @@ from src.model import train_loss, infer_loss
 
 
 # evaluate
-def evaluate(params, train_data, val_data, get_batch, config, steps=2):
-    batch_size = config['batch_size']
-    train_loss = [train_loss(params, get_batch(train_data, batch_size)) for _ in range(steps)]
-    val_loss = [infer_loss(params, get_batch(val_data, batch_size)) for _ in range(steps)]
-    return {'train_loss': np.mean(train_loss), 'val_loss': np.mean(val_loss)}
+def evaluate(params, train_data, val_data, get_batch, config, steps=4):
+    train_loss = train_loss(params, get_batch(train_data, config['batch_size'] * steps))
+    val_loss = infer_loss(params, get_batch(val_data, config['batch_size'] * steps))
+    return {'train_loss': train_loss, 'val_loss': val_loss}
 
 def save_if_best(params, best_loss, val_loss):
     if val_loss < best_loss:
