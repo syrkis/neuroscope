fbf8585af0bcfcacb1890db86c00178a44d4e3fd
Noah Syrkis <noah@syrkis.com>
2024-12-09 20:33:57 +0100

    more fun fun

--- app.py
+++
@@ -1,9 +0,0 @@
-# streamlit_app.py
-#     neuroscope project
-# by: Noah Syrkis
-
-# imports
-import streamlit as st
-
-st.title("Neuroscope")
-st.write("Welcome to Neuroscope, a web app that allows you to visualize your brain's activity in real time. To get started, please select a dataset from the sidebar.")
